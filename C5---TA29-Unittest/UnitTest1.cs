cda2f1618c1159858e920ee4e945aee229e94e70
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-23 15:59:58 +0200

    acabado

--- C5---TA29-Unittest/UnitTest1.cs
+++ C5---TA29-Unittest/UnitTest1.cs
@@ -1,15 +1,29 @@
-﻿namespace C5___TA29_Unittest;
+﻿namespace Geometria;
 
 public class Tests
 {
+    Geometria geo;
     [SetUp]
     public void Setup()
     {
+      geo = new Geometria();
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Assert.AreEqual(4,geo.areacuadrado(2));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(3.1416 * Math.Pow(1, 2), geo.areaCirculo(1));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.AreNotEqual("Default", geo.figura(8));
     }
 }
