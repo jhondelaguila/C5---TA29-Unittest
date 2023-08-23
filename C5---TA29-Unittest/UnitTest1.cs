b1afc95a5d46f1f0041da6e94e06ac46e32860ac
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-23 15:47:08 +0200

    np

---
+++ C5---TA29-Unittest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace C5___TA29_Unittest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
